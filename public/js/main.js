a5eb53d7182d36789dc1ffd5224c1a4072938f0b
unknown <maktabisaid1@gmail.com>
2021-03-17 16:04:24 +0100

    correction responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
 import {navLogo} from "./navbar.js";
 import {darkmode1,darkmode2} from "./darkmode.js";
 import {indicators,images} from "./carousel.js";
+import {signUp,register} from "./modal.js";
--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,5 +1,6 @@
 let navLogo = document.querySelector('#navLogo');
 let nav = document.querySelector('nav');
+let logo = document.querySelector('.empor');
 
 window.addEventListener('scroll', () => {
     if (window.pageYOffset >= '10') {
@@ -7,11 +8,13 @@ window.addEventListener('scroll', () => {
         navLogo.classList.remove('flex-column');
         navLogo.classList.add('justify-content-between');
         nav.style.marginTop = '0px';
+        logo.style.fontSize = '25px';
     }else{
         navLogo.classList.remove('fixed-top');
         navLogo.classList.add('flex-column');
         navLogo.classList.remove('justify-content-between');
         nav.style.marginTop = '80px';
+        logo.style.fontSize = '50px';
     }
 });
 
